4bc159a8d4ccfad9a6484b6538154d4d53dfd5cc
Osquel <77018049+osquelsantisteban@users.noreply.github.com>
2024-07-30 19:21:17 -0400

    remove comment

--- src/components/MyLocationBtn.vue
+++ src/components/MyLocationBtn.vue
@@ -20,7 +20,6 @@ const handleMoveMyLocation = () => {
 button{
     position: fixed;
     top: 80px;
-    /* right: 30px; */
     left: 6px;
     background-color: rgba(10,82,197,0.8);
     color: white;
@@ -32,19 +31,10 @@ button{
     box-shadow: rgba(0, 0, 0, 0.5) 0px 10px 15px;
     cursor: pointer;
     @media(min-width: 800px){
-        position: fixed;
         top: 15px;
         right: 15px;
         left: auto;
-        background-color: rgba(10,82,197,0.8);
-        color: white;
         padding: 0.5rem 0.75rem;
-        border-radius: 50%;
-        border: 0px;
-        -moz-box-shadow: rgba(0, 0, 0, 0.5) 0px 10px 15px;
-        -webkit-box-shadow: rgba(0, 0, 0, 0.5) 0px 10px 15px;
-        box-shadow: rgba(0, 0, 0, 0.5) 0px 10px 15px;
-        cursor: pointer;
     }
 }
 
@@ -53,8 +43,7 @@ button{
   0%   {    transform: scale(1);  }
   5%   {    transform: scale(0.9);  }
   25%   {    transform: scale(1.05);  }
-  100%   {    transform: scale(1.2);  }
-  
+  100%   {    transform: scale(1.2);  }  
 }
 
 button:hover{
@@ -67,22 +56,4 @@ img{
     width: 1.5rem;
     height: 1.75rem;
 }
-
-/* @media screen and (min-width: 800px) {
-    button{
-        position: fixed;
-        top: 15px;
-        right: 15px;
-        left: auto;
-        background-color: rgba(10,82,197,0.8);
-        color: white;
-        padding: 0.5rem 0.75rem;
-        border-radius: 50%;
-        border: 0px;
-        -moz-box-shadow: rgba(0, 0, 0, 0.5) 0px 10px 15px;
-        -webkit-box-shadow: rgba(0, 0, 0, 0.5) 0px 10px 15px;
-        box-shadow: rgba(0, 0, 0, 0.5) 0px 10px 15px;
-        cursor: pointer;
-    }
-} */
 </style>
